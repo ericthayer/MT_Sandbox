15591e5d542ffe8cfdf4602a0b82b5e91e3552c9
Eric Thayer <eric.thayer594@gmail.com>
2015-07-06 21:37:01 -0600

    initial commit

---
+++ DEV/js/script.js
@@ -0,0 +1,44 @@
+$(document).ready(function() {
+	// Flash Player z-index fix
+  window.onload = function () {
+    var frames = document.getElementsByTagName("iframe");
+    for (var i = 0; i < frames.length; i++) {
+      frames[i].src += "?wmode=transparent";
+    }
+  }
+  // Mobile Nav
+  var navigation = responsiveNav(".nav-collapse", {
+    animate: true,                    // Boolean: Use CSS3 transitions, true or false
+    transition: 500,                  // Integer: Speed of the transition, in milliseconds
+    label: "Menu",                    // String: Label for the navigation toggle
+    insert: "after",                  // String: Insert the toggle before or after the navigation
+    customToggle: "",                 // Selector: Specify the ID of a custom toggle
+    closeOnNavClick: true,           // Boolean: Close the navigation when one of the links are clicked
+    openPos: "relative",              // String: Position of the opened nav, relative or static
+    navClass: "nav-collapse",         // String: Default CSS class. If changed, you need to edit the CSS too!
+    navActiveClass: "js-nav-active",  // String: Class that is added to <html> element when nav is active
+    jsClass: "js-nav",                    // String: 'JS enabled' class which is added to <html> element
+    init: function(){},               // Function: Init callback
+    open: function(){},               // Function: Open callback
+    close: function(){}               // Function: Close callback
+  });
+  // Slider
+  $("#owl-demo").owlCarousel({
+	  //autoPlay : true,
+	  items : 4,
+	  lazyLoad : true,
+    navigation : true,
+    slideSpeed : 500,
+    paginationSpeed : 400,
+    singleItem:true, 
+  });
+  $("#owl-demo-two").owlCarousel({
+	  //autoPlay : true,
+	  items : 4,
+	  lazyLoad : true,
+    navigation : true,
+    slideSpeed : 500,
+    paginationSpeed : 400,
+    singleItem:true, 
+  });  
+});
